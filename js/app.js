12cdafd0a5e762032bc3374810421b3218de0306
Eric Smolen <esmolen2@gmail.com>
2018-06-02 20:35:02 -0400

    feat: Create cat class, instantiate cats, count clicks on their pics

--- js/app.js
+++ js/app.js
@@ -1,11 +1,58 @@
-const catPic = document.querySelector('.cat-picture');
+// Create variable for where the cat profiles will appear
+const catsContainer = document.getElementById('cats-container');
 
-const meowsDisplay = document.querySelector('.clicks');
-let meowsCount = 0;
+class Cat {
+	constructor(name, image) {
+		this.name = name;
+		this.image = image;
+		this.meowsCount = 0;
 
-function addMeow() {
-	meowsCount += 1;
-	meowsDisplay.innerHTML = meowsCount;
+		// Create Cat Profile Div
+		this.catProfile = document.createElement('div');
+		this.catProfile.setAttribute('id', this.name);
+		this.catProfile.setAttribute('class', 'cat');
+
+		// Add Cat Name header to profile
+		this.catName = document.createElement('h2');
+		this.catName.setAttribute('class', 'cat-name');
+		this.catName.innerHTML = this.name;
+		this.catProfile.appendChild(this.catName);
+
+		// Add Cat Meows counter to profile
+		this.catMeows = document.createElement('h3');
+		this.catClicks = document.createElement('span');
+		this.catClicks.setAttribute('class', 'clicks');
+		this.catClicks.innerHTML = this.meowsCount;
+		this.catMeows.appendChild(this.catClicks);
+		this.catMeows.insertAdjacentHTML('beforeend', ' Meows');
+		this.catProfile.appendChild(this.catMeows);
+
+		// Add Cat image to profile
+		this.catImage = document.createElement('img');
+		this.catImage.setAttribute('class', 'cat-picture');
+		this.catImage.src = this.image;
+		this.catProfile.appendChild(this.catImage);
+
+		// Add Cat Profile to Cats Container in DOM
+		catsContainer.appendChild(this.catProfile);
+	}
+
+	// Count the cats meows when clicked
+	addMeow() {
+		this.meowsCount += 1;
+		this.catClicks.innerHTML = this.meowsCount;
+	};
 }
 
-catPic.addEventListener('click', addMeow);
+// Create cats and count the clicks on their pictures
+const terrance = new Cat('Terrance', 'img/YawningKitten.jpg');
+terrance.catImage.addEventListener('click', function() {
+		terrance.addMeow();
+	}
+);
+
+const bisquick = new Cat('Bisquick', 'img/SurprisedCat.jpg');
+bisquick.catImage.addEventListener('click', function() {
+		bisquick.addMeow();
+	}
+);
