4334c90868037c2c44456692334749b980597461
Eric Smolen <esmolen2@gmail.com>
2018-06-11 21:16:07 -0400

    refactor: Organized code according to MVC model

--- js/app.js
+++ js/app.js
@@ -1,182 +1,152 @@
 // MODEL
-// Cats object where we can add cats info that will be used in the loop below to create cats
-const catsInfo = {
-	'catOne': {
-		'name': 'Terrance',
-		'image': 'img/YawningKitten.jpg',
-		'meowsCount': 0
-	},
+const model = {
+	currentCat: null,
 
-	'catTwo': {
-		'name': 'Bisquick',
-		'image': 'img/SurprisedCat.jpg',
-		'meowsCount': 0
-	},
+	catsInfo: [
+		{
+			'name': 'Terrance',
+			'image': 'img/YawningKitten.jpg',
+			'meowsCount': 0
+		},
 
-	'catThree': {
-		'name': 'Georgie',
-		'image': 'img/CuriousCat.jpeg',
-		'meowsCount': 0
-	},
+		{
+			'name': 'Bisquick',
+			'image': 'img/SurprisedCat.jpg',
+			'meowsCount': 0
+		},
 
-	'catFour': {
-		'name': 'Raul',
-		'image': 'img/FeistyCat.jpg',
-		'meowsCount': 0
-	},
+		{
+			'name': 'Georgie',
+			'image': 'img/CuriousCat.jpeg',
+			'meowsCount': 0
+		},
 
-	'catFive': {
-		'name': 'Bernard',
-		'image': 'img/SleepyCat.jpeg',
-		'meowsCount': 0
-	}
+		{
+			'name': 'Raul',
+			'image': 'img/FeistyCat.jpg',
+			'meowsCount': 0
+		},
+
+		{
+			'name': 'Bernard',
+			'image': 'img/SleepyCat.jpeg',
+			'meowsCount': 0
+		}
+	]
 };
 
-// OCTOPUS
+// CONTROLLER
+const controller = {
 
-function getNestedValue(obj, key) {
-	return key.split(".").reduce(function(result, key) {
-	   return result[key]
-	}, obj);
-}
+	init: function() {
+		model.currentCat = model.catsInfo[0];
 
-const catListListener = function(item) {
-	item.addEventListener('click', function() {
-		console.log(item.innerHTML);
-		return item.innerHTML;
-		// renderCatProfile(item.innerHTML);
-	});
-};
+		catProfileView.init();
+		catListView.init();
+	},
 
-(function createCatList() {
-	const catNameArray = [];
+	getCurrentCat: function() {
+		return model.currentCat;
+	},
 
-	for (var cat in catsInfo) {
-		if (catsInfo.hasOwnProperty(cat)) {
-			const nameProp = cat + '.name';
-			const catName = getNestedValue(catsInfo, nameProp);
-			catNameArray.push(catName);
-			};
+	setCurrentCat: function(cat) {
+		model.currentCat = cat;
+	},
+
+	increaseMeowsCount: function() {
+		model.currentCat.meowsCount += 1;
+		catProfileView.render();
+	},
+
+	getCatsList: function() {
+		return model.catsInfo;
 	}
-
-	renderCatList(catNameArray);
-})();
-
-// class Cat {
-// 	constructor(name, image) {
-// 		this.name = name;
-// 		this.image = image;
-// 		this.meowsCount = 0;
-// 	}
-//
-// 	// Count the cats meows when clicked
-// 	addMeow() {
-// 		this.meowsCount += 1;
-// 		// this.catClicks.innerHTML = this.meowsCount;
-// 	};
-//
-// 	displayCat() {
-// 		this.catProfile.classList.add('show-cat');
-// 	}
-//
-// 	hideCat() {
-// 		this.catProfile.classList.remove('show-cat');
-// 	}
-//
-// 	addToCatDisplayArray () {
-// 		catDisplay.push(this);
-// 	}
-//
-// 	removeFromCatDisplayArray () {
-// 		catDisplay.splice(0, 1);
-// 	}
-//
-// }
-
-// Function used to parse the value of a cat property in the catsInfo object using a string
-// function getNestedValue(obj, key) {
-//     return key.split(".").reduce(function(result, key) {
-//        return result[key]
-//     }, obj);
-// }
-
-// Loop through the catsInfo object, instantiate the cats and add to list, and add event listeners to profile to count clicks and to list to display cat
-// for (var cat in catsInfo) {
-// 	if (catsInfo.hasOwnProperty(cat)) {
-// 		const nameProp = cat + '.name';
-// 		const catName = getNestedValue(catsInfo, nameProp);
-// 		const imageProp = cat + '.image';
-// 		const catImage = getNestedValue(catsInfo, imageProp);
-//
-// 		const newCat = new Cat(catName, catImage);
-// 		// newCat.catImage.addEventListener('click', function() {
-// 		// 	newCat.addMeow();
-// 		});
-// 		// newCat.catListItem.addEventListener('click', function() {
-// 		// 	if (catDisplay < 1) {
-// 		// 		newCat.addToCatDisplayArray();
-// 		// 		newCat.displayCat();
-// 		// 	}	else if (newCat !== catDisplay[0]) {
-// 		// 		catDisplay[0].hideCat();
-// 		// 		catDisplay[0].removeFromCatDisplayArray();
-// 		// 		newCat.addToCatDisplayArray();
-// 		// 		newCat.displayCat();
-// 		// 	}
-// 		// });
-// 	}
-// }
-
+};
 
 // VIEW
-// Create Cat Profile Div
-function renderCatList(list) {
-	const catListDisplay = document.getElementById('cats-list').querySelector('ul');
+const catProfileView = {
 
-	list.forEach(function(cat) {
-		const catListItem = document.createElement('li');
-		catListItem.innerHTML = cat;
-		catListListener(catListItem);
-		catListDisplay.appendChild(catListItem);
-	});
-};
+	init: function() {
+		this.catsContainer = document.getElementById('cats-container');
 
-function renderCatProfile(name) {
-	const catProfile = document.createElement('div');
-	catProfile.setAttribute('id', name);
-	catProfile.setAttribute('class', 'cat');
+		// Create cat profile container
+		this.catProfile = document.createElement('div');
+		this.catProfile.setAttribute('class', 'cat');
+
+		// Create cat name header
+		this.catName = document.createElement('h2');
+		this.catName.setAttribute('class', 'cat-name');
+		this.catProfile.appendChild(this.catName);
+
+		// Create cat meows counter
+		this.catMeows = document.createElement('h3');
+		this.catClicks = document.createElement('span');
+		this.catClicks.setAttribute('class', 'clicks');
+		this.catMeows.appendChild(this.catClicks);
+		this.catMeows.insertAdjacentHTML('beforeend', ' Meows');
+		this.catProfile.appendChild(this.catMeows);
+
+		// Create cat image container
+		this.catImage = document.createElement('img');
+		this.catImage.setAttribute('class', 'cat-picture');
+		this.catProfile.appendChild(this.catImage);
+
+		// Add Cat Profile to Cats Container in DOM
+		this.catsContainer.appendChild(this.catProfile);
+
+		// Add event listener to count meows
+		this.catImage.addEventListener('click', function() {
+			controller.increaseMeowsCount();
+		});
+
+		this.render();
+	},
+
+	render: function() {
+		// Grab current cat info
+		this.currentCat = controller.getCurrentCat();
+
+		// Set cat profile id to current cat name
+		this.catProfile.setAttribute('id', this.currentCat.name);
+
+		// Set cat name header to current cat name
+		this.catName.innerHTML = this.currentCat.name;
+
+		// Set cat meows to current cat meows count
+		this.catClicks.innerHTML = this.currentCat.meowsCount;
+
+		// Set cat image to current cat image
+		this.catImage.src = this.currentCat.image;
+	}
 }
 
-// this.catProfile = document.createElement('div');
-// this.catProfile.setAttribute('id', this.name);
-// this.catProfile.setAttribute('class', 'cat');
-//
-// // Add Cat Name header to profile
-// this.catName = document.createElement('h2');
-// this.catName.setAttribute('class', 'cat-name');
-// this.catName.innerHTML = this.name;
-// this.catProfile.appendChild(this.catName);
-//
-// // Add Cat Meows counter to profile
-// this.catMeows = document.createElement('h3');
-// this.catClicks = document.createElement('span');
-// this.catClicks.setAttribute('class', 'clicks');
-// this.catClicks.innerHTML = this.meowsCount;
-// this.catMeows.appendChild(this.catClicks);
-// this.catMeows.insertAdjacentHTML('beforeend', ' Meows');
-// this.catProfile.appendChild(this.catMeows);
-//
-// // Add Cat image to profile
-// this.catImage = document.createElement('img');
-// this.catImage.setAttribute('class', 'cat-picture');
-// this.catImage.src = this.image;
-// this.catProfile.appendChild(this.catImage);
-//
-// // Add Cat Profile to Cats Container in DOM
-// catsContainer.appendChild(this.catProfile);
-//
+const catListView = {
 
+	init: function() {
+		this.catListDisplay = document.getElementById('cats-list').querySelector('ul');
 
-// Create variable for where the cat profiles will appear
-// const catsContainer = document.getElementById('cats-container');
-// const catsList = document.getElementById('cats-list').querySelector('ul');
-// const catDisplay = [];
+		this.render();
+	},
+
+	render: function() {
+		this.catListDisplay.innerHTML = '';
+
+		const catsList = controller.getCatsList();
+
+		catsList.forEach(function(cat) {
+			const catListItem = document.createElement('li');
+			catListItem.innerHTML = cat.name;
+			catListView.catListDisplay.appendChild(catListItem);
+
+			catListItem.addEventListener('click', (function(catCopy) {
+				return function() {
+					controller.setCurrentCat(catCopy);
+					catProfileView.render();
+				};
+			})(cat));
+		});
+
+	}
+}
+
+controller.init();
