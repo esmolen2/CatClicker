4c5eecf3c7e0550224c0d962be650b1e1e0baab4
Eric Smolen <esmolen2@gmail.com>
2018-06-12 18:20:08 -0400

    feat: Show/hide admin panel when admin and cancel buttons are clicked

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 const model = {
 	currentCat: null,
 
+	adminDisplay: false,
+
 	catsInfo: [
 		{
 			'name': 'Terrance',
@@ -43,6 +45,7 @@ const controller = {
 
 		catProfileView.init();
 		catListView.init();
+		adminView.init();
 	},
 
 	getCurrentCat: function() {
@@ -60,6 +63,20 @@ const controller = {
 
 	getCatsList: function() {
 		return model.catsInfo;
+	},
+
+	getAdminDisplay: function() {
+		return model.adminDisplay;
+	},
+
+	showAdminDisplay: function() {
+		model.adminDisplay = true;
+		adminView.render();
+	},
+
+	hideAdminDisplay: function() {
+		model.adminDisplay = false;
+		adminView.render();
 	}
 };
 
@@ -149,4 +166,59 @@ const catListView = {
 	}
 }
 
+const adminView = {
+
+	init: function() {
+		// Grab all the elements in the admin panel
+		this.adminButton = document.querySelector('#admin-mode');
+		this.adminForm = document.querySelector('#admin-form');
+		this.nameInput = document.querySelector('#name-input');
+		this.urlInput = document.querySelector('#url-input');
+		this.meowsInput = document.querySelector('#meows-input');
+		this.saveButton = document.querySelector('#save-button');
+		this.cancelButton = document.querySelector('#cancel-button');
+
+		// Show admin panel when admin button is clicked
+		this.adminButton.addEventListener('click', function() {
+			controller.showAdminDisplay();
+		});
+
+		// Hide admin panel when cancel button is clicked
+		this.cancelButton.addEventListener('click', function() {
+			controller.hideAdminDisplay();
+		});
+
+		this.render();
+	},
+
+	render: function() {
+		this.adminDisplay = controller.getAdminDisplay();
+
+		// If adminDisplay in model is set to false, clear form inputs and hide the admin panel. If it is set to true, show the panel
+		if(!this.adminDisplay) {
+			this.nameInput.value = '';
+			this.urlInput.value = '';
+			this.meowsInput.value = '';
+			this.adminForm.classList.add('hide-admin');
+
+		} else {
+			this.adminForm.classList.remove('hide-admin');
+		}
+	}
+}
+
 controller.init();
+
+// const saveButton = document.querySelector('#save-button');
+// const nameInput = document.querySelector('#name-input');
+// const urlInput = document.querySelector('#url-input');
+// const meowsInput = document.querySelector('#meows-input');
+//
+// function adminSave(e) {
+// 	e.preventDefault();
+// 	console.log(nameInput.value + urlInput.value + meowsInput.value);
+// }
+//
+// saveButton.addEventListener('click', function(event) {
+// 	adminSave(event);
+// });
